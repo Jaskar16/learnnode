3e7097da25f54b82b26dbfe74dea44d32fba905a
Jaskar Oppar <jaskaroppar@gmail.com>
2025-09-22 15:25:32 +0300

    midagi veel

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-console.log('Hellou World');
-var foo = 'Hellou';
-var bar = 'World';
+let num = 1;
+console.log(num);
+console.log(typeof num);
 
-console.log(foo); //old way dont use! hoisting
-console.log(bar);
+num = 1.2;
+console.log(num);
+console.log(typeof num);
+
+let text = 'Hello world';
+console.log(text);
+console.log(typeof text);
+
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+
+let nil = null;
+console.log(nil);
+console.log(typeof nil);
+
+let array = [1, 2, 'hello', 'tibu', true, [1, 2, 3]]
+console.log(array);
+console.log(typeof array);
+
+let obj = {
+    name: 'Jassu',
+    age: 67,
+    hairColor: 'Ginger',
+    isFemboy: true,
+};
+console.log(obj);
+console.log(typeof obj);
