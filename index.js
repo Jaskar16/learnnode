9090d41d743f7e9403ab00658820460f6a7722f4
Jaskar Oppar <jaskaroppar@gmail.com>
2025-09-25 14:51:31 +0300

    function

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
-let obj = {
-    name: 'Jaskar',
-    age: 67,
-    looks: 'CL',
-    fruits: ['mango', 'banana', 'kiwi'],
-    hiir: {
-        age: 1,
-        name: 'Cornelius',
-        gender: 'male'
-    },
-    ParentName: 'Frederik',
-    a2sd: 'asd',
-    'parent-name': 'Name',
-    'a2sd': 'asd',
-    'some thing': 'asd2',
-};
+function hello() {
+    console.log('Hello!');
+}
+hello()
+hello()
 
-console.log(obj);
-console.log(obj.name);
-console.log(obj.fruits[1]);
-console.log(obj.hiir.name);
-console.log(obj['parent-name']);
-console.log(Object.keys(obj));
+function helloName(name) {
+    console.log(`Hello ${name}!`);
+}
+helloName('jaskar');
+helloName('Karl');
+helloName('Frederik');
+
+function hi(name = 'Nameless', age = 'infinite') {
+    console.log(`Hi ${name} you are ${age} years old`);
+}
+hi('Jaskar', 67);
+hi('Karl', 67);
+hi('Frederik', 67);
+hi();
+
+let sum = function (a, b) {
+    return a + b;
+}
+let answer = sum(4, 5);
+console.log(answer);
+console.log(sum(6, 10));
+sum(5, 6);
+
+let sub = (a, b) => {
+    return a - b;
+}
+
+console.log(sub(6, 10));
+
+let mult = (a, b) => a * b;
+
+console.log(mult(6, 10));
+
+let root = a => a + a;
+
+console.log(root(6));
+
+let cool = () => console.log('cool');
+
+cool();
+
+let awesome = {
+    party: function () {
+        console.log('PARTY!');
+    },
+    cool: () => console.log('cool'),
+    nice() {
+        console.log('nice');
+    }
+}
+
+awesome.party();
+awesome.cool();
+awesome.nice();
