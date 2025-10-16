c668135808f3c7ab516434c0a63db0723789a51a
Jaskar Oppar <jaskaroppar@gmail.com>
2025-10-16 15:13:50 +0300

    add tab

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup>
 import { ref } from 'vue';
-import Modal from './components/Modal.vue';
+import Tabs from './components/tabs.vue';
+let tabTitles = ['Pictures', 'Music', 'Videos', 'Documents', 'Games'];
+let tabContent = [
+    'Some cool Pictures',
+    'Some cool Music',
+    'Some cool Videos',
+    'Some cool Documents',
+    'Some cool Games',
+];
+let tabActive = ref(0);
 
-let modalActive = ref(false);
+function setActive(key){
+    tabActive.value = key;
+}
+
 </script>
 <template>
-    <button class="button is-primary" @click="modalActive=true">Open Modal</button>
-    <Modal></Modal>
+    <Tabs :titles="tabTitles" @change="setActive"></Tabs>
+    <div class="content">
+        <h1>{{ tabContent[tabActive] }}</h1>
+    </div>
 </template>
