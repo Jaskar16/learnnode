cb111736c6d32392e4b4fa93fbc6159506699f2a
Jaskar Oppar <jaskaroppar@gmail.com>
2025-10-30 14:28:38 +0200

    finish cookie clicker

--- src/pages/RickandMorty.vue
+++ src/pages/RickandMorty.vue
@@ -10,21 +10,56 @@ let info = ref({
     next: null,
     prev: null,
 });
-async function getCharacters(url) {
-    let res = await axios.get(url);
+
+let currentPage = ref(1);
+let searchValue = ref('');
+
+async function getCharacters(page) {
+    let res = await axios.get('https://rickandmortyapi.com/api/character', {
+        params: {
+            page,
+            name: searchValue.value
+        }
+    });
     console.log(res);
-    characters.value = res.data.results;
+    characters.value.push(...res.data.results);
     info.value = res.data.info;
 }
 
-await getCharacters('https://rickandmortyapi.com/api/character');
+await getCharacters(currentPage.value);
+
+
+let timeout;
+
+async function getPage(page) {
+    clearTimeout(timeout);
+    timeout = setTimeout(async () => {
+        currentPage.value = page;
+        await getCharacters(currentPage.value);
+    }, 1000);
+}
+document.addEventListener('scroll', function() {
+    if(window.innerHeight + window.scrollY > document.body.clientHeight - 100) {
+        getPage(currentPage.value + 1);
+    }
+});
+
+
+
 
 </script>
 <template>
-    <div class="my-2 is-flex is-justify-content-space-between">
-        <button class="button is-primary" :disabled="!info.prev" @click="getCharacters(info.prev)">Prev</button>
-        <button class="button is-primary" :disabled="!info.next" @click="getCharacters(info.next)">Next</button>
+    <div class="field has-addons">
+        <div class="control is-expanded">
+            <input v-model="searchValue" @input="getPage(1)" class="input" type="text">
+        </div>
+        <div class="control">
+            <button class="button is-info" @click="getPage(1)">
+                Search
+            </button>
+        </div>
     </div>
+   
     <div class="columns is-multiline">
         <div class="column is-3" v-for="character in characters">
             <CharacterCard :character="character"></CharacterCard>
