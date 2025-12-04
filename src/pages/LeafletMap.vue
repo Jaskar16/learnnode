c0f747bd9c154dfc8f2364fa4f35fe85bf4046e2
Jaskar Oppar <jaskaroppar@gmail.com>
2025-12-04 14:37:53 +0200

    add go home

--- src/pages/LeafletMap.vue
+++ src/pages/LeafletMap.vue
@@ -15,6 +15,11 @@ let coords = ref({
         {{ zoom }}   
         <button class="button is-primary" @click="coords={lat: 59.41641, lng:  24.79871}">Go to Airport</button>
     </div>
+    <div>
+        <input v-model="zoom" type="range" max="19" min="1" step="1">
+        {{ zoom }}   
+        <button class="button is-primary" @click="coords={lat: 59.31125, lng:  24.40120}">Go to Home</button>
+    </div>
     <Leaflet :lat="coords.lat" :lng="coords.lng" :zoom="zoom"></Leaflet>
     <Leaflet :lat="24.74344" :lng="59.42690" :zoom="5"></Leaflet>
 </template>
